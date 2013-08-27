96890a35cd383f717e8e7420b44f1a78f4617c50
Naeem <naeem.muhammad@libis.kuleuven.be>
2013-08-27 14:56:46 +0200

    logging partial

--- dmt.php
+++ dmt.php
@@ -1,7 +1,9 @@
 <?php
+    require_once('util/KLogger.php');
 	require_once("dmtservice.php");
 	require_once("util/dmtdatafiles.php");
 
+    $log = KLogger::instance('logging/', KLogger::DEBUG);
 	$dmtservice = new service;
 
     ob_start();
@@ -10,6 +12,10 @@
 	$requestmethod = $_SERVER['REQUEST_METHOD'];
 	$requestPath = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
 	$requestParameters = $_SERVER['QUERY_STRING'];
+
+    $requestLogId = round(microtime(true) * 1000).'-'.$_SERVER['REMOTE_ADDR'];
+
+    $log->logInfo($requestLogId.'->Request Type:'. $_SERVER['REQUEST_METHOD']);
 
 	$arrResponse = "";
 	switch ($requestmethod) {
@@ -20,6 +26,8 @@
 		
 	  case 'POST':
 		if($dmtservice -> validRequest($requestPath)){
+
+            $log->logInfo($requestLogId.'->Is a valid Request.');
 
 			if (isset($_POST['sourceFormat']) && isset($_POST['targetFormat'])){
 				$responseFile = new DataFile();
@@ -46,6 +54,9 @@
                     //http_response_code(422);
                     header("Status: 422 Unprocessable Entity");
                     $arrResponse = "Record(s) is not provided";
+
+                    $log->logError($requestLogId.'->'.$arrResponse);
+
                     exit(json_encode ($arrResponse));
                 }
                 if (isset($_FILES['record']) && isset($_FILES['records']))
@@ -53,10 +64,14 @@
                     //http_response_code(404);
                     header("Status: 404 Not Found");
                     $arrResponse = "record and records parameters cannot be handled together in one request.";
+                    $log->logError($requestLogId.'->'.$arrResponse);
                     exit(json_encode ($arrResponse));
                 }
 
                 $recordFile = $dmtservice -> dmtDirectory($contentRecord, $nameRecord, $typeRecord, $requestPath[2]);
+
+                $log->logInfo($requestLogId.'->Request Directory on Server:'.$recordFile->filePath);
+
                 if (isset($recordFile))
                 {
 
@@ -65,6 +80,9 @@
                         if($_FILES['mappingRulesFile']['type'] == 'text/csv'){
                             $arrResponse = $dmtservice -> recordMapping($rulesContent,$_FILES['mappingRulesFile']['name'],
                                 $recordFile, $sourceFormat, $targetFormat);
+
+                            $log->logInfo($requestLogId.'->Request ID:'.$arrResponse);
+
                             header('Content-Type: application/json');
                             exit(json_encode (array('request_id' => $arrResponse)));
                         }
@@ -72,6 +90,9 @@
                             //http_response_code(422);
                             header("Status: 422 Unprocessable Entity");
                             $arrResponse = "Input mapping rules file is not a CSV file";
+
+                            $log->logError($requestLogId.'->'.$arrResponse);
+
                             $dmtservice -> removeDirectory($recordFile->filePath);
                             exit(json_encode ($arrResponse));
                         }
@@ -96,9 +117,15 @@
                                 //http_response_code(422);
                                 header("Status: 422 Unprocessable Entity");
                                 $arrResponse = 'Unsupported source format.';
+
+                                $log->logError($requestLogId.'->'.$arrResponse);
+
                                 $dmtservice -> removeDirectory($recordFile->filePath);
                                 exit(json_encode ($arrResponse));
                         }
+
+                        $log->logInfo($requestLogId.'->Mapping file (XLT) Used:'.$mappingFile->fileName);
+
                         $arrResponse = $dmtservice -> recordTransformation($recordFile, $mappingFile);
                         header('Content-Type: application/json');
                         exit(json_encode (array('request_id' => $arrResponse)));
@@ -108,6 +135,9 @@
                     //http_response_code(404);
                     header("Status: 404 Not Found");
                     $arrResponse = "Error in storing record file(s).";
+
+                    $log->logError($requestLogId.'->'.$arrResponse);
+
                     exit(json_encode ($arrResponse));
                 }
 
@@ -117,6 +147,8 @@
                 //http_response_code(404);
                 header("Status: 404 Not Found");
 				$arrResponse = "Please provide both source and target formats";
+
+                $log->logError($requestLogId.'->'.$arrResponse);
 			}
 
 		}else
@@ -124,22 +156,30 @@
             //http_response_code(422);
             header("Status: 422 Unprocessable Entity");
 			$arrResponse = "Invalid URL. <br> Please provide url in '/DataMapping/provider/batch/action?parameters' format.";
+            $log->logError($requestLogId.'->'.$arrResponse);
         }
 
 		break;
 		
 	  case 'GET':
           if($dmtservice -> validRequest($requestPath)){
+              $log->logInfo($requestLogId.'->Is a valid Request.');
 
               switch(strtoupper($requestPath[3])){
                   case 'STATUS':
                       if(isset($_GET['request_id'])){
                           header('Content-Type: application/json');
                           $arrResponse = array('status_code' => $dmtservice->getStatus($_GET['request_id']));
+
+                          $log->logInfo($requestLogId.'->Resuest Status:', $arrResponse);
+
                       }
                       else{
                           $arrResponse = 'request_id is needed for '.strtoupper($requestPath[3]).' actions.';
                           //http_response_code(422);
+
+                          $log->logError($requestLogId.'->'.$arrResponse);
+
                           header("Status: 422 Unprocessable Entity");
                       }
                       break;
@@ -150,6 +190,8 @@
                           if(isset($resultFile)){
                               $file = $resultFile -> filePath."/".$resultFile -> fileName;
                               if (file_exists($file)) {
+                                  $log->logInfo($requestLogId.'->Result file sent:'.$file);
+
                                   header('Content-Description: File Transfer');
                                   header('Content-Type: '. $resultFile -> fileType);
                                   header('Content-Disposition: attachment; filename='.basename($file));
@@ -168,6 +210,8 @@
                       else{
                           $arrResponse = 'a valid request_id is needed for '.strtoupper($requestPath[3]).' actions.';
                           //http_response_code(422);
+                          $log->logError($requestLogId.'->'.$arrResponse);
+
                           header("Status: 422 Unprocessable Entity");
                       }
                       break;
@@ -175,12 +219,17 @@
                   case 'LIST':
                       header('Content-Type: application/json');
                       $arrResponse =  $dmtservice->getSupportedFormatList();
+
+                      $log->logInfo($requestLogId.'->List of supported formats',$arrResponse);
+
                       exit(json_encode ($arrResponse));
                       break;
 
                   default:
                       $arrResponse = 'Request '.strtoupper($requestPath[3]).' is not supported.';
                       //http_response_code(404);
+                      $log->logError($requestLogId.'->'.$arrResponse);
+
                       header("Status: 404 Not Found");
                       //exit();
               }
@@ -190,12 +239,14 @@
               //http_response_code(422);
               header("Status: 422 Unprocessable Entity");
               $arrResponse = "Invalid URL. <br> Please provide url in '/DataMapping/provider/batch/action?parameters' format.";
+              $log->logError($requestLogId.'->'.$arrResponse);
               //exit();
           }
 		break;
 		
 	  case 'DELETE':
-		$arrResponse = "Delete Request"; 
+		$arrResponse = "Delete Request";
+        $log->logInfo($requestLogId.'->'.$arrResponse);
 		break;
 
 	}
